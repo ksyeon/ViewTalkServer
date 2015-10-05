026801f121c0b3e5e172dc622364fe7277582cfc
Serenity <kim910320@naver.com>
2015-10-05 23:41:28 +0900

    - Json 관련 클래스 추가

---
+++ WakeUpMessangerServer/Modules/JsonHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeUpMessangerServer.Modules
+{
+    public class JsonName
+    {
+        public const string ID = "ID";
+        public const string Password = "Password";
+    }
+
+    public class JsonHelper
+    {
+        public JsonHelper()
+        {
+
+        }
+
+        public Dictionary<string, string> GetLoginInfo(string data)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            JsonParser jsonParser = new JsonParser(data);
+
+            dictionary.Add(JsonName.ID, jsonParser.GetStringValue(JsonName.ID));
+            dictionary.Add(JsonName.Password, jsonParser.GetStringValue(JsonName.Password));
+
+            return dictionary;
+        }
+    }
+}
---
+++ WakeUpMessangerServer/Modules/JsonParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Json;
+
+namespace WakeUpMessangerServer.Modules
+{
+    public class JsonParser
+    {
+        private JsonTextParser jsonTextParser;
+        private JsonObjectCollection jsonObjectCollection;
+
+        public JsonParser(string data)
+        {
+            this.jsonTextParser = new JsonTextParser();
+            this.jsonObjectCollection = (JsonObjectCollection)jsonTextParser.Parse(data);
+        }
+
+        public string GetStringValue(string name)
+        {
+            string value = string.Empty;
+
+            try
+            {
+                value = Convert.ToString(jsonObjectCollection[name].GetValue());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return value;
+        }
+
+        public string[] GetStringArrayValue(string name)
+        {
+            string[] arrayValue = null;
+
+            try
+            {
+                JsonArrayCollection jsonArrayCollection = (JsonArrayCollection)jsonObjectCollection[name];
+                int count = jsonArrayCollection.Count;
+
+                arrayValue = new string[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    arrayValue[i] = ((JsonStringValue)jsonArrayCollection[i]).Value;
+                }     
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return arrayValue;
+        }
+    }
+}
